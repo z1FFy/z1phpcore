309098ababff2e38a7f4436a55adbd4726b39a66
d.kuschenko <ziffyweb@gmail.com>
2016-11-12 03:14:45 +0300

    Changes

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
 require_once ('app/config.php'); // Include configuration file
 require_once ('core/functions.php'); //Include functions
 require_once ('core/z1core.php'); // Include framework library file
-$loader = require __DIR__ . '/vendor/autoload.php';
-
 
 $z1 = new z1Core(); // Create spec of class z1Core Class
 $z1->run($config); // Main func. which start process of running web site
